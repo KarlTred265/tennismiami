e061af092976e14d56329705446301211a67662d
KarlTred265 Tred <tred26559@gmail.com>
2023-06-20 21:33:15 +0000

    added more

--- userapp/urls.py
+++ userapp/urls.py
@@ -2,7 +2,12 @@ from django.urls import path
 from userapp import views
 
 urlpatterns = [
-    path('',views.Home,name="Home")
+    path('',views.Home,name="Home"),
+    path('signup',views.signup,name="signup"),
+    path('login',views.handlelogin,name="handlelogin"),
+    path('logout',views.handleLogout,name="handleLogout"),
+    path('contact',views.contact,name="contact"),
+    path('join',views.enroll,name="enroll"),
 
 
 ]
