c0b9f11cdef1ec9bab46cf30253376637866f87f
KarlTred265 Tred <tred26559@gmail.com>
2023-06-21 09:52:59 +0000

    add all

--- userapp/urls.py
+++ userapp/urls.py
@@ -8,6 +8,8 @@ urlpatterns = [
     path('logout',views.handleLogout,name="handleLogout"),
     path('contact',views.contact,name="contact"),
     path('join',views.enroll,name="enroll"),
+    path('profile',views.profile,name="profile"),
+    path('attendance',views.attendance,name="attendance"),
 
 
 ]
